c6bfe5370b50057c8f4a3335492002b0664129ab
shabaz ahmad <shabazahmad786mughal@gmail.com>
2025-07-01 01:51:30 +0530

    Final fix: Corrected wsgi.py indentation

--- chinar_project/wsgi.py
+++ chinar_project/wsgi.py
@@ -1,16 +1,20 @@
+# F:\chinar\chinar_project\wsgi.py
+
 """
 WSGI config for chinar_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Set the default Django settings module for the 'chinar_project' project.
+# This is crucial for Gunicorn to find your settings.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinar_project.settings')
 
 application = get_wsgi_application()
